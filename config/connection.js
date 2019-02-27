29662d05e5727218e6a5258628abc320a2ac8fb2
Monetthigpen <monet.thigpen@gmail.com>
2019-02-27 18:31:10 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log("connection.JAWSDB_URL")
+}
+else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "back8987",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
